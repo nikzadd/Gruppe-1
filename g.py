02b99c7efa6c442f76df919df735e47d3bf8e890
danieloverland <danieloverland@icloud.com>
2022-10-25 10:42:57 +0200

    g

--- g.py
+++ g.py
@@ -2,11 +2,8 @@ from f import lag_avtale
 def avtaler():
     avtaleliste = []
     avtaleliste.append(lag_avtale())
-    try:
-        for i in range(len(avtaleliste)):
-            print("Avtalenummer:", i+1, "Tittel:", avtaleliste[i].tittel)
-    except ValueError:
-        pass
+    for i in range(len(avtaleliste)):
+        print("Avtalenummer:", i+1, "Tittel:", avtaleliste[i].tittel)
 avtaler()
 
 
