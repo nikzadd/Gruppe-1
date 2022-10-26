103efdbabe48432da442f4333176f1a38123afbd
nikzadd <homan_97@hotmail.com>
2022-10-26 13:47:38 +0200

    Update d.py

--- d.py
+++ d.py
@@ -9,16 +9,16 @@ class Avtale:
         self.starttidspunkt = starttidspunkt
         self.varighet = varighet
     def __str__(self):
-        return 'Avtale ' +str(self.tittel)+'/'+str(self.sted)+'/'+datetime(self.starttidspunkt)+':'+int(self.varighet)
+        return 'Avtale ' +str(self.tittel)+'/'+str(self.sted)+'/'+int(self.starttidspunkt)+':'+int(self.varighet)
 
 
 tittel = input("Skriv inn avtale")
 sted = input("Vennligst skriv sted")
 sted = str(sted)
 starttidspunkt = input("Vennligst skriv starttidspunkt")
-starttidspunkt = datetime(starttidspunkt)
+starttidspunkt = int(starttidspunkt)
 varighet = input("Vennligst skriv inn varighet")
-verighet = int(varighet)
+varighet = int(varighet)
 
 DinAvtale = Avtale(tittel,sted,starttidspunkt,varighet)
 print(DinAvtale)
