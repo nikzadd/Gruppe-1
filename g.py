82314a7c2aaaafbfc92bdc1dd84e63634f503d74
danieloverland <danieloverland@icloud.com>
2022-10-26 12:35:10 +0200

    update

--- g.py
+++ g.py
@@ -2,7 +2,8 @@ from f import lag_avtale
 def avtaler(avtaleliste, overskrift="Avtale:"):
     print(overskrift)
     for i in range(len(avtaleliste)):
-        print("Avtalenummer:", i+1, "Tittel:", avtaleliste[i].tittel)
+        print("Avtalenummer:", i+1, "Tittel:", avtaleliste[i].tittel, "Sted:",
+              avtaleliste[i].sted, "Varighet:", avtaleliste[i].varighet, "Starttidspunkt:", avtaleliste[i].starttidspunkt)
 avtale_tom = []
 avtale_tom.append(lag_avtale())
 avtaler(avtale_tom)
--- h.py
+++ h.py
@@ -1,6 +1,6 @@
 from g import avtaler
-def avtale_til_tekstfil(avtalefil):
-    for linje in avtaler():
-        avtalefil.write(linje + "\n")
+def avtale_til_tekstfil(avtaler):
+    for linje in avtaler:
+        avtale_til_tekstfil.write(str((i+1, avtaler[linje].tittel, avtaler[linje].sted,
+            avtaler[linje].varighet, avtaler[linje].starttidspunkt + "\n"))
 avtalefil = open("avatalefil.txt", "w", encoding="UTF8")
-
