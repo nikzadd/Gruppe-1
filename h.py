f8975552f6f3c747d36a5225031f6d65031810af
danieloverland <danieloverland@icloud.com>
2022-10-27 14:39:56 +0200

    Update h.py

--- h.py
+++ h.py
@@ -4,6 +4,6 @@ def avtale_til_tekstfil(avtaler):
     avtalefil = open("avatalefil.txt", "a", encoding="UTF8")
     for avtale in avtaler:
         #avtalefil.write()
-        avtalefil.write(str(str(i+1)[0] + "; " + avtale[1].tittel + "; " + avtale[2].sted + "; " +
-             str(avtale[3].starttidspunkt) + "; " + str(avtale[4].varighet) + "\n"))
+        avtalefil.write(str(str(i+1) + "; " + avtale.tittel + "; " + avtale.sted + "; " +
+             str(avtale.starttidspunkt) + "; " + str(avtale.varighet) + "\n"))
         i = i+1
