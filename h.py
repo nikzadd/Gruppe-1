d726f8a8d30b4f27510ac1f2b0ba6e6c3e5eaf4f
siverttilset <116627109+siverttilset@users.noreply.github.com>
2022-10-27 15:07:22 +0200

    ab

--- h.py
+++ h.py
@@ -5,4 +5,5 @@ def avtale_til_tekstfil(avtaler):
     for avtale in avtaler:
         avtalefil.write(str(str(i+1) + "; " + avtale.tittel + "; " + avtale.sted + "; " +
              str(avtale.starttidspunkt) + "; " + str(avtale.varighet) + "\n"))
+        avtalefil.write("{}/{}/{}/{}/{}\n".format("Avtalenummer", avtale.tittel, avtale.sted, avtale.starttidspunkt, avtale.varighet))
         i = i+1
