0c7138413bd6e72dd44faebc157912336bc39bf8
siverttilset <116627109+siverttilset@users.noreply.github.com>
2022-10-27 15:39:18 +0200

    a

--- h.py
+++ h.py
@@ -1,7 +1,7 @@
 
 def avtale_til_tekstfil(avtaler):
     i = 0
-    avtalefil = open("avtalefil.txt", "w", encoding="UTF8")
+    avtalefil = open("avtalefil.txt", "a", encoding="UTF8")
     for avtale in avtaler:
         avtalefil.write(str(str(i+1) + "; " + avtale.tittel + "; " + avtale.sted + "; " +
              str(avtale.starttidspunkt) + "; " + str(avtale.varighet) + "\n"))
