d5ca7d348c75ed70aa03bfb376e5b1ba7517bd6b
André Tangen <116624799+andretangen99@users.noreply.github.com>
2022-10-27 15:20:36 +0200

    Update j.py

--- j.py
+++ j.py
@@ -1,8 +1,20 @@
 import datetime
 from g import avtale_tom
-from f import 
+from datetime import datetime
+
+
 
 def liste_avtaler(avtale_tom, dato):
-    avtaler = []
-    avtaler = avtale_tom
-    return 
+    liste_avtaler = []
+    for i in avtale_tom:
+        if dato == avtale_tom[2]:
+            avtale_tom[0].append = liste_avtaler
+    return liste_avtaler
+
+#test
+if __name__ == "__main__":
+    dato_sjekk = input("Skriv inn dato: ")
+    dato = datetime.fromisoformat(dato_sjekk)
+    test = liste_avtaler(avtale_tom, dato)
+    print(test)
+
