ed915913a33d759ada333d4d20e140d6392e23e2
André Tangen <116624799+andretangen99@users.noreply.github.com>
2022-10-28 10:49:07 +0200

    no gutta!

--- j.py
+++ j.py
@@ -1,19 +1,10 @@
 from datetime import datetime
+from f import lag_avtale
+from g import avtale_tom
 
-
-#Hent fra avtalefil
-
-def liste_sjekk(liste_avtaler, dato):
+def liste_sjekk(avtale_tom, dato):
     liste_avtaler = []
-    with open("avtalefil.txt", "r", encoding="UTF8") as avtalefil:
-        for linje in avtalefil:
-            if dato == dato:
-                linje.append = liste_avtaler
+    for avtale in avtale_tom:
+        if avtale == dato:
+            lag_avtale.tittel.append(liste_avtaler)
     return liste_avtaler
-
-#test
-if __name__ == "__main__":
-    dato = input("Skriv inn dato på formen YYYY-MM-DD XX:XX:XX: ")
-    #dato = datetime.fromisoformat(dato_sjekk)
-    test = liste_sjekk(liste_sjekk, dato)
-    print(test)
