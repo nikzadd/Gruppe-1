ce587214c616020bf22c4238bc59d60ed035e329
danieloverland <danieloverland@icloud.com>
2022-11-02 10:14:58 +0100

    g

--- j.py
+++ j.py
@@ -10,25 +10,7 @@ def avtale_sjekk(avtale_liste, dato):
                 Avtale.tittel.append(liste_avtaler_samtidig)
     return liste_avtaler_samtidig
 
-<<<<<<< HEAD
-#Hent fra avtalefil
-from f import lag_avtale
 
-def liste_sjekk(liste_avtaler, dato):
-    liste_avtaler = []
-    with open("avtalefil.txt", "r", encoding="UTF8") as avtalefil:
-        for linje in avtalefil:
-            if dato == dato:
-                linje.append = liste_avtaler
-    return liste_avtaler
-
-#test
-if __name__ == "__main__":
-    dato = input("Skriv inn dato på formen YYYY-MM-DD XX:XX:XX: ")
-    #dato = datetime.fromisoformat(dato_sjekk)
-    test = liste_sjekk(liste_sjekk, dato)
-    print(test)
-=======
 # if __name__ == "__main__":
 #     avtale_liste = []
 #     avtale_liste.append(lag_avtale())
@@ -37,4 +19,4 @@ if __name__ == "__main__":
 #     dato_sjekk = input("Sjekk dato: ")
 #     dato_1 = datetime.fromisoformat(dato_sjekk)
 #     avtale_sjekk(avtale_liste, dato_1)
->>>>>>> 78e79ee672a8a14e0d60d4d88223aa4b134e60b2
+
