54cae9cca20f5f431b00dd24406c62de95931570
nikzadd <homan_97@hotmail.com>
2022-11-03 08:59:58 +0100

    Create d2.py

---
+++ d2.py
@@ -0,0 +1,27 @@
+# d
+
+class Kategori:
+    def __init__(self, id, navn, prioritet):
+        i = 1
+        self.id = id
+        self.navn = navn
+        self.prioritet = prioritet
+        
+    def __str__(self):
+        return 'Kategori ' +str(self.id)+'/'+str(self.navn)+'/'+int(self.prioritet)
+        
+    
+    def __str__ (self):
+        return f"id: {self.id}, navn: {self.navn}, prioritet: {self.prioritet} "
+
+
+def lag_kategori():
+    id = input("Skriv inn ID: ")
+    navn = str(input("Skriv navn: "))
+    prioritet = int(input("Skriv inn prioritet. Skriv på format: "))
+    prioritet = i = 1
+    Dinkategori = Kategori(id, navn, prioritet)
+    return Dinkategori
+
+if __name__ =="__main__":
+    lag_kategori()
