307fc98e6f5e6d33f8d895fa692b2f956a1bb8d4
danieloverland <danieloverland@icloud.com>
2022-11-10 10:35:59 +0100

    Update Oving10.py

--- Oving10.py
+++ Oving10.py
@@ -27,19 +27,20 @@ def lag_kategori():
 
 #e
 
-def kategorier(avtaleliste, overskrift="Kategori:"):    
+def kategorier(avtaleliste, overskrift="Kategori:"):
     i = 0
     print(overskrift)
     for linje in range(len(avtaleliste)):
-        print("ID:", avtaleliste[i].id, "Navn:", avtaleliste[i].navn, "Prioritet:",
+        print("Indeks:", i+1, "ID:", avtaleliste[i].id, "Navn:", avtaleliste[i].navn, "Prioritet:",
               avtaleliste[i].prioritet)
         i = i+1
 
 def kategori_til_tekstfil(avtaler):
     kategorifil = open("kategorifil.txt", "w", encoding="UTF8")
-    kategorifil.write("ID: " + "Navn: " + "Prioritet: " + "\n")
+    kategorifil.write("Indeks: " + "ID: "  + "Navn: " + "Prioritet: " + "\n")
     for avtale in avtaler:
         kategorifil.write(str(str(avtale.id) + "; " + str(avtale.navn) + "; " + str(avtale.prioritet) + "; " +"\n"))
+
 
 if __name__ =="__main__":
     kategori_tom = []
